dd10deed2bbe5b6549d3616c6fcc2ab15ce46e18
Gerlo Hesselink <gerlo.hesselink@ordina.nl>
2015-07-01 16:09:51 +0200

    better Generics handling

--- src/main/java/nl/ordina/reactive/circuitbreaker/CircuitBreakerProducer.java
+++ src/main/java/nl/ordina/reactive/circuitbreaker/CircuitBreakerProducer.java
@@ -2,6 +2,7 @@ package nl.ordina.reactive.circuitbreaker;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
 import javax.annotation.Resource;
 import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.enterprise.inject.Produces;
@@ -12,21 +13,22 @@ public class CircuitBreakerProducer {
     @Resource(lookup = "concurrent/myExecutor")
     private ManagedExecutorService exs;
 
-    //This is not ideal, CircuitBreaker may be created twice but the last created CircuitBreaker will be used.
-    private static final Map<String, CircuitBreaker> circuitBreakers = new ConcurrentHashMap<>();
+    //This is not ideal, CircuitBreaker for a Caller class may be created twice but the last created CircuitBreaker will be used.
+    private static final Map<String, CircuitBreaker<?>> circuitBreakers = new ConcurrentHashMap<>();
 
-    @Produces
+	@Produces
     public <T> CircuitBreaker<T> createCircuitBreaker(InjectionPoint injectionPoint) {
         String key = injectionPoint.getBean().getBeanClass().getName();
-        CircuitBreaker cb = circuitBreakers.get(key);
+        @SuppressWarnings("unchecked")
+		CircuitBreaker<T> cb = (CircuitBreaker<T>) circuitBreakers.get(key);
         if (cb != null) {
             return cb;
         }
         CircuitBreakerConfig config = injectionPoint.getAnnotated().getAnnotation(CircuitBreakerConfig.class);
         if (config == null) {
-            cb = new CircuitBreakerImpl(exs);
+            cb = new CircuitBreakerImpl<T>(exs);
         } else {
-            cb = new CircuitBreakerImpl(exs, config.timeOut(), config.sleepWindow(), config.errorsThreshold());
+            cb = new CircuitBreakerImpl<T>(exs, config.timeOut(), config.sleepWindow(), config.errorsThreshold());
         }
         circuitBreakers.put(key, cb);
         return cb;
